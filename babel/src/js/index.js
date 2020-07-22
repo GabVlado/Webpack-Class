22d3ec34a0917d1f43cb2a298719ba29e6f02641
Gabo Vlad <gv.valdivieso2309@gmail.com>
2020-07-22 13:59:57 -0500

    babel loader ,plugins

---
+++ babel/src/js/index.js
@@ -0,0 +1,14 @@
+import '../css/index.css'
+import search from './search'
+import render from './render'
+
+
+const id = prompt('pokemon')
+
+search(id)
+    .then((data) => {
+        render(data)
+    })
+    .catch(() => {
+        console.error('Error:')
+    })
